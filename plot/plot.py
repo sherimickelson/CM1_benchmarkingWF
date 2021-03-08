ebfe107b7774061965deb99338539338aecb2912
sherimickelson <mickelso@ucar.edu>
2021-03-08 12:03:08 -0700

    changed to relative difference instead of absolute

--- plot/plot.py
+++ plot/plot.py
@@ -22,7 +22,9 @@ def plot_it(json_file):
         fns_raw = f.read()
     fns = json.loads(fns_raw)
     print('Using these log files ....')
-    for f in sorted(fns.keys()):
+    print("control log file: "+fns["control"])
+    print("experiment log files:")
+    for f in sorted(fns["experiment"].keys()):
         print(f)
     print("\n")
 
@@ -31,11 +33,21 @@ def plot_it(json_file):
     values = {}
     for p in plots:
         values[p] = OrderedDict()
-        for fn in sorted(fns.keys()):
+        values[p][fns["control"]]=OrderedDict()
+        for fn in sorted(fns["experiment"].keys()):
             values[p][fn]=OrderedDict()
 
+    fn = fns["control"]
+    with open(fn) as f:
+        for l in f:
+            if 'Metric' in l:
+                values[l.split('Metric:')[-1].strip().split('=')[0].strip()][fn][ts] = float(l.split('=')[-1])
+                #print(str(ts)+" "+str(float(l.split('=')[-1])))
+            if 'nwritet =' in l:
+                ts = int(l.split('nwritet =')[-1].strip())            
+
     # parse out the values
-    for fn in sorted(fns.keys()):
+    for fn in sorted(fns["experiment"].keys()):
         with open(fn) as f:
             for l in f:
                 if 'Metric' in l:
@@ -50,7 +62,7 @@ def plot_it(json_file):
 #        print (p)
 #        for fn in values[p].keys():
 #            print('--------------------')
-#            print(fn + "  " + fns[fn])
+#            print(fn + "  " + fns["experiment"][fn])
 #            print(values[p][fn])
 
     # create plots
@@ -60,11 +72,12 @@ def plot_it(json_file):
 
     def plot(_plt, i):
         colors = []
-        for fn in sorted(fns.keys()):
-            if 'auto' in fns[fn]:
-                colors.append(_plt.plot(values[plots[i]][fn].keys(), values[plots[i]][fn].values())[0].get_color())
+        for fn in sorted(fns["experiment"].keys()):
+            res = {key: (values[plots[i]][fns["control"]][key]-values[plots[i]][fn][key])/values[plots[i]][fns["control"]][key] for key in values[plots[i]][fns["control"]].keys()}
+            if 'auto' in fns["experiment"][fn]:
+                colors.append(_plt.plot(values[plots[i]][fn].keys(), res.values())[0].get_color())
             else:
-                colors.append(_plt.plot(values[plots[i]][fn].keys(), values[plots[i]][fn].values(), color=fns[fn])[0].get_color())
+                colors.append(_plt.plot(values[plots[i]][fn].keys(), res.values())[0].get_color())
         _plt.set_xlabel('time (min)')
         _plt.set_ylabel(plots[i])
         _plt.grid(linestyle='--')
@@ -77,7 +90,7 @@ def plot_it(json_file):
     for c in colors:
         lines.append(mlines.Line2D([], [], color=c))
 
-    fig.legend(handles=lines, labels=sorted(fns.keys()), loc = "lower center")
+    fig.legend(handles=lines, labels=sorted(fns["experiment"].keys()), loc = "lower center")
     plt.subplots_adjust(bottom=0.25)
 
     plt.show()
