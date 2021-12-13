d2831ad7af145c14b100eeec522835f75a19245e
sherimickelson <mickelso@ucar.edu>
2021-12-13 16:09:16 -0700

    Add date to log files.  Reformat the file.json fie.  Add stat comparison.

--- plot/plot.py
+++ plot/plot.py
@@ -4,8 +4,37 @@ import json
 import numpy as np
 from collections import OrderedDict
 
+def parse_it(log_file):
 
-def plot_it(json_file):
+    # parse the log file
+    info = {
+              'stat':{},
+              'metric':{},
+              'performance':{}
+    }
+    with open(log_file) as cfile:
+        for line in cfile:
+            if "stat::" in line:
+                split = line.strip().replace("stat::","").split(':')
+                #print(split)
+                if split[0].strip() in info['stat'].keys():
+                    info['stat'][split[0].strip()].append(split[1].split()[0].strip())
+                else:
+                    info['stat'][split[0].strip()] = [split[1].split()[0].strip()]
+                if len(split) > 2:
+                    if split[1].split()[-1].strip() in info['stat'].keys():
+                        info['stat'][split[1].split()[-1].strip()].append(split[2].split()[0].strip())
+                    else:
+                        info['stat'][split[1].split()[-1].strip()] = [split[2].split()[0].strip()]
+#    for v in info['stat'].keys():
+#        print("\n"+log_file+"\n"+v+": size "+str(len(info['stat'][v])))
+#        print(info['stat'][v])
+
+
+    return info
+
+
+def read_logs(json_file):
     #  Grep for these strings in the files specified within the json file
     # Metric: LWP (g/m2)
     # Metric: cloud fraction (%)     
@@ -21,28 +50,55 @@ def plot_it(json_file):
     with open(json_file) as f:
         fns_raw = f.read()
     fns = json.loads(fns_raw)
-    print('Using these log files ....')
+   
+    total_ok = 0
+    total_fail = 0
+ 
+    # loop through the different sets of log files
     for f in sorted(fns.keys()):
-        print(f)
-    print("\n")
 
+        print("Working on the log files under: "+f)
+        print("Using control file:"+fns[f]['control'])
+        
+        ok = 0
+        fail = 0
 
-    # setup the dict that will hold all of the values
-    values = {}
-    for p in plots:
-        values[p] = OrderedDict()
-        for fn in sorted(fns.keys()):
-            values[p][fn]=OrderedDict()
+        # parse the control file
+        control = parse_it(fns[f]['control'])
+        experiment = parse_it(fns[f]['experiment'])
+        # compare this against the control
+        for v in control['stat'].keys():
+            if control['stat'][v] != experiment['stat'][v]:
+                print("\nDIFFERENCE IN VARIABLE "+v)
+                print(control['stat'][v])
+                print(experiment['stat'][v])
+                fail+=1
+            else:
+                print("\nNO DIFFERENCE IN VARIABLE "+v)
+                ok+=1
+        # Print a summary for this comparison
+        print("============================================")
+        print("Summary for "+fns[f]['control']+" compared to "+fns[f]['experiment'])
+        print(str(ok)+" stat variables are ok.")
+        print(str(fail)+" stat variables show a difference.")
+        print("\n\n")
+        total_ok = total_ok+ok
+        total_fail = total_fail+fail
 
-    # parse out the values
-    for fn in sorted(fns.keys()):
-        with open(fn) as f:
-            for l in f:
-                if 'Metric' in l:
-                    values[l.split('Metric:')[-1].strip().split('=')[0].strip()][fn][ts] = float(l.split('=')[-1])
-                    #print(str(ts)+" "+str(float(l.split('=')[-1])))
-                if 'nwritet =' in l:
-                    ts = int(l.split('nwritet =')[-1].strip())
+    # Print a summary across all comparisons
+    print("============================================")
+    print("Summary for all comparisons")
+    print(str(total_ok)+" stat variables are ok.")
+    print(str(total_fail)+" stat variables show a difference.") 
+    print("\n\n")
+
+        # what are the log files to plot
+
+        # create a dictionary that holds parsed values for each of the log files in the list
+        # this list will have the plot values
+        # we also need a list to compare the stat values and performance numbers
+
+# grep for "stat::"
 
 # DEBUG
 #    for p in values.keys():
@@ -53,34 +109,38 @@ def plot_it(json_file):
 #            print(fn + "  " + fns[fn])
 #            print(values[p][fn])
 
-    # create plots
-    fig, ((pl1, pl2), (pl3,pl4)) = plt.subplots(2, 2, figsize=(12, 10))
-    plts = [pl1, pl2, pl3, pl4]
+        # create plots
+#        fig, ((pl1, pl2), (pl3,pl4)) = plt.subplots(2, 2, figsize=(12, 10))
+#        plts = [pl1, pl2, pl3, pl4]
 
 
-    def plot(_plt, i):
-        colors = []
-        for fn in sorted(fns.keys()):
-            if 'auto' in fns[fn]:
-                colors.append(_plt.plot(values[plots[i]][fn].keys(), values[plots[i]][fn].values())[0].get_color())
-            else:
-                colors.append(_plt.plot(values[plots[i]][fn].keys(), values[plots[i]][fn].values(), color=fns[fn])[0].get_color())
-        _plt.set_xlabel('time (min)')
-        _plt.set_ylabel(plots[i])
-        _plt.grid(linestyle='--')
-        return colors
+#  Need to find a better way to collese all of the information within one location!!!!!!!
 
-    for i in range(0, len(plts)):
-        colors = plot(plts[i], i)
-
-    lines = []
-    for c in colors:
-        lines.append(mlines.Line2D([], [], color=c))
-
-    fig.legend(handles=lines, labels=sorted(fns.keys()), loc = "lower center")
-    plt.subplots_adjust(bottom=0.25)
-
-    plt.show()
+#    def plot(_plt, i):
+#        colors = []
+#        for fn in sorted(fns.keys()):
+#            if 'auto' in fns[fn]:
+#                colors.append(_plt.plot(values[plots[i]][fn].keys(), values[plots[i]][fn].values())[0].get_color())
+#            else:
+#                colors.append(_plt.plot(values[plots[i]][fn].keys(), values[plots[i]][fn].values(), color=fns[fn])[0].get_color())
+#        _plt.set_xlabel('time (min)')
+#        _plt.set_ylabel(plots[i])
+#        _plt.grid(linestyle='--')
+#        return colors
+#
+#    for i in range(0, len(plts)):
+#        colors = plot(plts[i], i)
+#
+#    lines = []
+#    for c in colors:
+#        lines.append(mlines.Line2D([], [], color=c))
+#
+#    fig.legend(handles=lines, labels=sorted(fns.keys()), loc = "lower center")
+#    plt.subplots_adjust(bottom=0.25)
+#
+#    plt.show()
 
 if __name__ == '__main__':
-    plot_it("files.json")
+    read_logs("files.json")
+
+
